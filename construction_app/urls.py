8a563828f5b6b5bebcaf5e64e5f9c628a986679b
Abinjayathilakan <abinjayathilakan@gmail.com>
2023-06-05 22:27:21 +0530

    first commit

--- construction_app/urls.py
+++ construction_app/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('',views.navbar,name='navbar')
+    path('',views.navbar,name='navbar'),
+    path('homepage',views.homepage,name='homepage'),
+    path('experties',views.experties,name='experties'),
+    path('productbrand',views.productbrand,name='productbrand'),
+    path('footer',views.footer,name='footer'),
+    path('buyproducts',views.buyproducts,name='buyproducts'),
+    path('gallery',views.gallery,name='gallery'),
+    
+    path('career',views.career,name='career'),
+    path('contact',views.contact,name='contact'),
+    path('projectupdates',views.projectupdates,name='projectupdates'),
+    path('ongoingprojects',views.ongoingprojects,name='ongoingprojects'),
 ]
--- construction_app/views.py
+++ construction_app/views.py
@@ -2,4 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 def navbar(request):
-    return render(request,'navbar2.html')
+    return render(request,'navbar3.html')
+
+def homepage(request):
+    return render(request,'homepage.html')
+
+def experties(request):
+    return render(request,'experties.html')
+
+def productbrand(request):
+    return render(request,'productbrand.html')
+
+def footer(request):
+    return render(request,'footer.html')
+
+def buyproducts(request):
+    return render(request,'buyproducts.html')
+
+def gallery(request):
+    return render(request,'gallery.html')
+
+def career(request):
+    return render(request,'career.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def projectupdates(request):
+    return render(request,'projectupdates.html')
+
+def ongoingprojects(request):
+    return render(request,'ongoingprojects.html')
